6d74b9075db65fc6392bdf99a8193e40f8928a1c
FernandoG <67986168+gomezfernando1995@users.noreply.github.com>
2024-05-08 15:50:41 +0000

    service-worked

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,13 @@ chrome.runtime.onInstalled.addListener(function () {
     // Agrega el evento de escucha para cuando se selecciona una pestaña
     chrome.tabs.onActivated.addListener(function (activeInfo) {
         var tabId = activeInfo.tabId;
-
+        
+        // Obtener la información de la pestaña actual
         chrome.tabs.get(tabId, function (tab) {
-            var tabURL = tab.url;
-            console.log("La url es : " + tabURL);
-        })
+            var tabUrl = tab.url;
+            console.log("La URL de la pestaña seleccionada es: " + tabUrl);
+            // Puedes hacer lo que necesites con la URL de la pestaña aquí
+        });
         // Puedes hacer lo que necesites con el ID de la pestaña aquí
     });
 });
