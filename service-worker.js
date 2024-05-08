9ac3b9572a41a28aafb58cecf8e24b872ce42320
FernandoG <67986168+gomezfernando1995@users.noreply.github.com>
2024-05-08 20:48:22 +0000

    cambios

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 
 
 chrome.runtime.onInstalled.addListener(function () {
+    // Escucha el evento onActivated en chrome.tabs
     chrome.tabs.onActivated.addListener(function (activeInfo) {
         chrome.tabs.sendMessage(activeInfo.tabId, { action: "verificarLocalStorage" });
     });
-});
-
+})
