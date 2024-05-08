90f53a89af284feb98f6c5bc3e4350a9ff412642
FernandoG <67986168+gomezfernando1995@users.noreply.github.com>
2024-05-08 17:39:56 +0000

    seguimos

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,16 @@ chrome.runtime.onInstalled.addListener(function () {
     // Agrega el evento de escucha para cuando se selecciona una pestaña
     chrome.tabs.onActivated.addListener(function (activeInfo) {
         var tabId = activeInfo.tabId;
-        
+
         // Obtener la información de la pestaña actual
         chrome.tabs.get(tabId, function (tab) {
             var tabUrl = tab.url;
-            console.log("La URL de la pestaña seleccionada es: " + tabUrl);
+            var localURL= JSON.stringify(localStorage.getItem("url"));
+            if(tabUrl===localURL){
+                console.log("La URL de la pestaña seleccionada es: " + tabUrl);
+            }else{
+                console.log("no existe la url "+tabUrl);
+            }
             // Puedes hacer lo que necesites con la URL de la pestaña aquí
         });
         // Puedes hacer lo que necesites con el ID de la pestaña aquí
